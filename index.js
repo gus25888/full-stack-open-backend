10cad7ac5dd672e5cd5d1fd42d92d63168300fff
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2024-12-02 17:13:50 -0300

    Exercise 3.11 completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 // Config. logs para métodos que no son POST.
 app.use(morgan('tiny', {
@@ -24,22 +25,22 @@ app.use(morgan(
 
 let persons = [
     {
-        "id": 1,
+        "id": "1",
         "name": "Arto Hellas",
         "number": "040-123456"
     },
     {
-        "id": 2,
+        "id": "2",
         "name": "Ada Lovelace",
         "number": "39-44-5323523"
     },
     {
-        "id": 3,
+        "id": "3",
         "name": "Dan Abramov",
         "number": "12-43-234345"
     },
     {
-        "id": 4,
+        "id": "4",
         "name": "Mary Poppendieck",
         "number": "39-23-6423122"
     }
